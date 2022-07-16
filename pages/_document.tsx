b486a7c65942966cc6ff4eef936edfe772272fc1
Alberto Frattini <frattini.alberto@gmail.com>
2022-07-16 18:33:33 +0200

    eslint and cleaning for deployment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document(props) {
         <Html lang="en">
             <Head>
                 <link
-                    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700"
+                    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700&display=optional"
                     rel="stylesheet"
                 ></link>
                 <link
@@ -20,28 +20,6 @@ export default function Document(props) {
                     rel="preconnect"
                     href="https://fonts.gstatic.com/"
                     crossOrigin=""
-                />
-                <link
-                    href="/static/favicons/apple-touch-icon.png"
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                />
-                <link
-                    href="/static/favicons/favicon-32x32.png"
-                    rel="icon"
-                    sizes="32x32"
-                    type="image/png"
-                />
-                <link
-                    href="/static/favicons/favicon-16x16.png"
-                    rel="icon"
-                    sizes="16x16"
-                    type="image/png"
-                />
-                <link
-                    color="#4a9885"
-                    href="/static/favicons/safari-pinned-tab.svg"
-                    rel="mask-icon"
                 />
             </Head>
             <body>
