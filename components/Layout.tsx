7fbd200e895dbaa3dee1c99544eac7907889db60
Alberto Frattini <frattini.alberto@gmail.com>
2022-07-16 23:21:46 +0200

    Added title to Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
     return (
         <div className="bg-gray-50 dark:bg-gray-900">
+            <Head>
+                <title>Alberto Frattini - javascript developer</title>
+            </Head>
             <Header />
             <main className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900">
                 {children}
